dedb6aaf4e1453f76733e4b561484daa806832bf
ddWayontec <demisangdan@gmail.com>
2018-11-30 18:30:00 -0700

    added rent out space page, linked from settings page

---
+++ app/src/main/java/com/codingwithmitch/googlemaps2018/ui/RentOutActivity.java
@@ -0,0 +1,14 @@
+package com.codingwithmitch.googlemaps2018.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.codingwithmitch.googlemaps2018.R;
+
+public class RentOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_out);
+    }
+}
--- app/src/main/java/com/codingwithmitch/googlemaps2018/ui/SettingsActivity.java
+++ app/src/main/java/com/codingwithmitch/googlemaps2018/ui/SettingsActivity.java
@@ -1,14 +1,29 @@
 package com.codingwithmitch.googlemaps2018.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.codingwithmitch.googlemaps2018.R;
 
+import android.view.View;
+import android.widget.Button;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button rentOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        rentOutButton = (Button) findViewById(R.id.rentOutButton);
+
+        rentOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, RentOutActivity.class));
+            }
+        });
     }
 }
