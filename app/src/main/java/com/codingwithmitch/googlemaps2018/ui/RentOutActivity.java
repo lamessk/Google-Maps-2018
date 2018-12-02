649389eb8c5dc478af7527d62f4c3e4d80fe645f
ddWayontec <demisangdan@gmail.com>
2018-12-01 17:05:31 -0700

    added ParkingSpot java class for creating a parking spot

--- app/src/main/java/com/codingwithmitch/googlemaps2018/ui/RentOutActivity.java
+++ app/src/main/java/com/codingwithmitch/googlemaps2018/ui/RentOutActivity.java
@@ -2,13 +2,34 @@ package com.codingwithmitch.googlemaps2018.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.codingwithmitch.googlemaps2018.R;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 public class RentOutActivity extends AppCompatActivity {
+
+    private EditText pAddress, pDescription;
+    private Button regParkingSpotButton;
+
+    private FirebaseFirestore pDb;
+
+
+    public void registerNewParkingLocation(final String parkingAddress, final String parkingDescription){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_out);
+
+
     }
 }
